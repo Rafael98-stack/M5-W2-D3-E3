c9463a9ea46752f1368e1ee0c84a6f2bd21c3806
Rafael98-stack <rafaelcasapaojr@gmail.com>
2024-02-14 23:03:15 +0100

    Developing

--- src/main/java/it/be/epicode/EsercizioTre/controllers/BlogsController.java
+++ src/main/java/it/be/epicode/EsercizioTre/controllers/BlogsController.java
@@ -1,6 +1,7 @@
 package it.be.epicode.EsercizioTre.Controllers;
 
 import it.be.epicode.EsercizioTre.Entities.BlogPost;
+import it.be.epicode.EsercizioTre.PayLoad.BlogPostPayLoad;
 import it.be.epicode.EsercizioTre.Services.AutorsBlogsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +14,7 @@ public class BlogsController {
     @Autowired
     private AutorsBlogsService autorsBlogsService;
 
-//    @PostMapping
-//    // POST su http://localhost:3001/examples/payloadExample (+body)
-//    public Blog_Post payloadExample(@RequestBody Blog_Post body) {
-//        System.out.println("body --> " + body);
-//        return body;
-//    }
+
 
     @GetMapping
     public List<BlogPost> getAllBlogs() {
@@ -26,8 +22,7 @@ public class BlogsController {
     }
 
     @PostMapping
-  public BlogPost saveBlogs() {
-
-        return this.autorsBlogsService.saveBlogs(new BlogPost());
+  public BlogPost saveBlogs(@RequestBody BlogPostPayLoad body) {
+        return this.autorsBlogsService.saveBlogs(body);
     }
 }
